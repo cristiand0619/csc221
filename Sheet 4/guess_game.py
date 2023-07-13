11f2d4a6130e2f009049a6ba7b5c280a3c27d562
Cristian Diaz <cad90073@email.vccs.edu>
2023-07-13 10:27:43 -0400

    Add counter

--- Sheet 4/guess_game.py
+++ Sheet 4/guess_game.py
@@ -4,12 +4,15 @@ number = randint(1, 6)
 print("OK, I've thought of a number between 1 and 6.\n")
 prompt = "Make a guess: "
 answer = -1
+counter = 0
 
 while answer != number:
     answer = int(input(prompt))
+    counter = counter + 1
     if answer < number:
         print("That's too low.\n")
     elif answer > number:
         print("That's too high.\n")
 
 print("That was my number. Well done!")
+print(f'\nYou took {counter} guesses.')
