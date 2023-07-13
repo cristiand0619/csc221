49a032091965e6edd8c48665e900f97ab6cb846a
Cristian Diaz <cad90073@email.vccs.edu>
2023-07-13 11:01:54 -0400

    Finish program

--- Sheet 4/guess_game.py
+++ Sheet 4/guess_game.py
@@ -1,18 +1,26 @@
 from random import randint
 
-number = randint(1, 6)
-print("OK, I've thought of a number between 1 and 6.\n")
-prompt = "Make a guess: "
-answer = -1
-counter = 0
+top = 643
+again = "yes"
 
-while answer != number:
-    answer = int(input(prompt))
-    counter = counter + 1
-    if answer < number:
-        print("That's too low.\n")
-    elif answer > number:
-        print("That's too high.\n")
+while again == "yes":
+    number = randint(1, top)
+    print(f"OK, I've thought of a number between 1 and {top}.\n")
+    prompt = "Make a guess: "
+    answer = -1
+    counter = 0
 
-print("That was my number. Well done!")
-print(f'\nYou took {counter} guesses.')
+    while answer != number:
+        answer = int(input(prompt))
+        counter = counter + 1
+        if answer < number:
+            print("That's too low.\n")
+        elif answer > number:
+            print("That's too high.\n")
+
+    print("That was my number. Well done!")
+    print(f'\nYou took {counter} guesses.')
+    
+    again = input("Would you like another game? ")
+
+print("OK. Bye!")
